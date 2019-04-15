7136f78bf631eaa316d2652365f0aac670e4d736
Manuel Casupanan <manuel@tsuri.de>
2019-04-15 16:16:52 +0200

    This version is deprecated.

--- chatkit-app/src/config.js
+++ chatkit-app/src/config.js
@@ -1,5 +1,5 @@
 const tokenUrl =
-  "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/4e60549a-24d4-42d5-b612-19819f0dede3/token";
-const instanceLocator = "v1:us1:4e60549a-24d4-42d5-b612-19819f0dede3";
+  "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/558ec46c-064b-479e-8041-f9b404e7ba23/token";
+const instanceLocator = "v1:us1:558ec46c-064b-479e-8041-f9b404e7ba23";
 
 export { tokenUrl, instanceLocator };
